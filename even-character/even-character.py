ada766be672ea4e0ee90cbda87c3f84963355bad
Abishek <abishekk54@gmail.com>
2019-07-03 00:47:09 +0530

    even-filter completed 1.Comments need to be added

--- even-character/even-character.py
+++ even-character/even-character.py
@@ -1,20 +1,24 @@
-string = 'addafggffccddvvcc'#'asasaaaadfghj'
 def fun(string):
-	count = 0
-	index = 0
-	for i in range(0,len(string)-1):
-		print 'i'+str(i)	
-		if string[i]==string[i+1]:
-			count +=1
-			index = i
-		elif string[i]!=string[i+1] and count>0:
-			break
-	print count, index
-	if (count%2)!=0:
-		print 'str1:'+string[:index-count+1]
-		print 'str2:'+string[index+2:]
-		fun(string[:index-count+1] + string[index+2:])
-	else:	
-		print string
-print string
-fun(string)
+	k=0
+	while(k<len(string)):
+		count = 1
+		for i in range(k,len(string)-1):
+			if string[i] == string[i+1]:
+				count += 1
+			else:
+				break
+		if count%2 == 0 and i+count != len(string):
+			string = string[:i-count+1] + string[i+1:]
+			k =-1
+		elif count%2 == 0:
+			string = string[:i-count+2] + string[i+2:]
+			k =-1
+		else:
+			k += count-1
+		k+=1
+	print(string)
+
+fun('abbbaacd')
+fun('addafggffccddvvcc')
+fun('asasaaaadfghj')
+fun('aa')
